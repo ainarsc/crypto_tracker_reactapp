c162a1ffc4603b9297442caccc3713e212250fad
Ainars Ciesa <ainars.c@gmail.com>
2020-01-15 14:54:18 +0000

    Refactor into seperate files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React, { Fragment } from "react";
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import ResponsiveDrawer from "./components/navigation/SideDrawer";
+import NavBar from "./components/navigation/NavBar";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex"
+  }
+}));
 
 function App() {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+  const classes = useStyles();
   return (
-    <Fragment>
-      <ResponsiveDrawer />
-      <h1>Foo</h1>
-    </Fragment>
+    <div className={classes.root}>
+      <CssBaseline />
+      <ResponsiveDrawer
+        handleDrawerToggle={handleDrawerToggle}
+        mobileOpen={mobileOpen}
+      />
+      <NavBar handleDrawerToggle={handleDrawerToggle} />
+    </div>
   );
 }
 
--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -1,46 +1,80 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
+import PropTypes from "prop-types";
 import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+import Link from "@material-ui/core/Link";
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1
+  appBar: {
+    [theme.breakpoints.up("sm")]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth
+    }
   },
   menuButton: {
-    marginRight: theme.spacing(2)
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      display: "none"
+    }
+  },
+  toolbar: {
+    // theme: theme.mixins.toolbar
+    marginLeft: theme.spacing(2)
   },
   title: {
     flexGrow: 1
   }
 }));
 
-function NavBar() {
+function NavBar(props) {
   const classes = useStyles();
+
+  const preventDefault = event => event.preventDefault();
+
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Dashboard
+    <AppBar position="fixed" className={classes.appBar}>
+      <Toolbar className={classes.toolbar}>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="start"
+          onClick={props.handleDrawerToggle}
+          className={classes.menuButton}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography className={classes.title} variant="h6" noWrap>
+          Dashboard
+        </Typography>
+        <Link
+          href="#"
+          className={classes.toolbar}
+          onClick={preventDefault}
+          color="inherit"
+        >
+          <Typography variant="h6" noWrap>
+            Link
           </Typography>
-          {false && <Button color="inherit">Login</Button>}
-        </Toolbar>
-      </AppBar>
-    </div>
+        </Link>
+      </Toolbar>
+    </AppBar>
   );
 }
+
+NavBar.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  container: PropTypes.instanceOf(
+    typeof Element === "undefined" ? Object : Element
+  )
+};
 
 export default NavBar;
