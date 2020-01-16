805aac007b9e2867f8f1f24f70a910635aa5984e
Ainars Ciesa <ainars.c@gmail.com>
2020-01-16 15:12:09 +0000

    Add grid components for dashboard content disyplay

---
+++ src/components/navigation/Main.js
@@ -0,0 +1,89 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+import clsx from "clsx";
+import Copyright from "../Copyright";
+import LineChart from "../charts/LineChart";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  content: {
+    flexGrow: 1,
+    height: "100vh",
+    overflow: "auto"
+  },
+  container: {
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2)
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: "flex",
+    overflow: "auto",
+    flexDirection: "column"
+  },
+  fixedHeight: {
+    height: 220
+  },
+  appBarSpacer: theme.mixins.toolbar
+}));
+
+export default function Main() {
+  const classes = useStyles();
+  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+
+  return (
+    <main className={classes.content}>
+      <div className={classes.appBarSpacer} />
+      <Container maxWidth="lg" className={classes.container}>
+        <Grid container spacing={2}>
+          <Grid item xs={2}>
+            <Paper className={classes.paper}>BTC</Paper>
+          </Grid>
+          <Grid item xs={2}>
+            <Paper className={classes.paper}>BTC</Paper>
+          </Grid>
+          <Grid item xs={2}>
+            <Paper className={classes.paper}>BTC</Paper>
+          </Grid>
+          <Grid item xs={2}>
+            <Paper className={classes.paper}>BTC</Paper>
+          </Grid>
+          <Grid item xs={2}>
+            <Paper className={classes.paper}>BTC</Paper>
+          </Grid>
+          <Grid item xs={2}>
+            <Paper className={classes.paper}>BTC</Paper>
+          </Grid>
+        </Grid>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={8} lg={9}>
+            <Paper className={fixedHeightPaper}>
+              <LineChart />
+            </Paper>
+          </Grid>
+
+          <Grid item xs={12} md={4} lg={3}>
+            <Paper className={fixedHeightPaper}>xs 12</Paper>
+          </Grid>
+
+          <Grid item xs={12} md={8} lg={9}>
+            <Paper className={fixedHeightPaper}>xs 12</Paper>
+          </Grid>
+
+          <Grid item xs={12} md={4} lg={3}>
+            <Paper className={fixedHeightPaper}>xs 12</Paper>
+          </Grid>
+        </Grid>
+        <Box pt={4}>
+          <Copyright />
+        </Box>
+      </Container>
+    </main>
+  );
+}
