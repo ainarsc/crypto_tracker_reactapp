2a343e4616ef56bdeefffd1d0a5bfcf12c60d014
Ainars Ciesa <ainars.c@gmail.com>
2020-01-29 15:16:10 +0000

    Generate multiple currency data

--- src/components/dashboard/PriceList.js
+++ src/components/dashboard/PriceList.js
@@ -4,31 +4,18 @@ import Paper from "@material-ui/core/Paper";
 import useDataFetch from "../utils/useDataFetch";
 
 const PriceList = ({ classNAME }) => {
-  const crypto = "BTC";
-  const currency = "USD";
-  const url = `https://min-api.cryptocompare.com/data/price?fsym=${crypto}&tsyms=${currency}`;
+  const url = `https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,XRP,BCH,BSV,LTC&tsyms=USD`;
   const [state] = useDataFetch(url);
 
   return (
     <Grid container spacing={2}>
-      <Grid item xs={2}>
-        <Paper className={classNAME}>{`BTC: $${state.data.USD}`}</Paper>
-      </Grid>
-      <Grid item xs={2}>
-        <Paper className={classNAME}>BTC</Paper>
-      </Grid>
-      <Grid item xs={2}>
-        <Paper className={classNAME}>BTC</Paper>
-      </Grid>
-      <Grid item xs={2}>
-        <Paper className={classNAME}>BTC</Paper>
-      </Grid>
-      <Grid item xs={2}>
-        <Paper className={classNAME}>BTC</Paper>
-      </Grid>
-      <Grid item xs={2}>
-        <Paper className={classNAME}>BTC</Paper>
-      </Grid>
+      {Object.keys(state.data).map(key => (
+        <Grid item xs={2}>
+          <Paper
+            className={classNAME}
+          >{`${key}: $${state.data[key].USD}`}</Paper>
+        </Grid>
+      ))}
     </Grid>
   );
 };
