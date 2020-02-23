cfa78be97e43fc6e1e516a67cd33ce0a47486bdb
Ainars Ciesa <ainars.c@gmail.com>
2020-02-23 19:24:46 +0000

    Extract raw data processing from the component, add redux state, clean up the code

--- src/components/dashboard/components/PriceList.js
+++ src/components/dashboard/components/PriceList.js
@@ -5,28 +5,30 @@ import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 import { connect } from "react-redux";
 import { fetchData } from "../../../actions/fetchData";
+import _ from "lodash";
 
 const PriceList = ({ styles, data, fetchData, ...props }) => {
-  const url = `https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,XRP,BCH,BSV,LTC&tsyms=USD`;
-
   useEffect(() => {
-    fetchData("PRICE", url);
-  }, [url, fetchData]);
+    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,BCH,BSV,LTC&&tsyms=USD`;
+    const keysToPick = ["FROMSYMBOL", "PRICE"];
+
+    fetchData("PRICE", url, "USD", keysToPick);
+  }, [fetchData]);
 
   return (
     data.PRICE !== undefined && (
       <Grid {...props}>
-        {Object.keys(data.PRICE.data).map((key, index) => (
+        {_.map(data.PRICE.data, (coin, index) => (
           <Grid key={index} item xs={4} md={2}>
             <Paper className={styles}>
-              <Typography variant="title">{`${key}`}</Typography>
+              <Typography variant="title">{`${coin.FROMSYMBOL}`}</Typography>
               <Divider
                 width="100%"
                 orientation="horizontal"
                 variant="middle"
                 flexItem
               />
-              <Typography variant="h6">{`$${data.PRICE.data[key].USD}`}</Typography>
+              <Typography variant="h6">{`$${coin.PRICE}`}</Typography>
             </Paper>
           </Grid>
         ))}
@@ -39,4 +41,8 @@ const mapState = state => ({
   data: state.dataByCategory
 });
 
-export default connect(mapState, { fetchData })(PriceList);
+const mapActions = {
+  fetchData
+};
+
+export default connect(mapState, mapActions)(PriceList);
