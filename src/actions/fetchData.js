7f0df2debb69e70e1e0b3aa6a69284b0a99eb321
Ainars Ciesa <ainars.c@gmail.com>
2020-02-26 02:05:10 +0000

    Tweak api fetching actions and data processing function

--- src/actions/fetchData.js
+++ src/actions/fetchData.js
@@ -5,7 +5,7 @@ import {
   FETCH_FAIL
 } from "./actionTypes";
 import axios from "axios";
-import { cleanseDataProto } from "../utils/cleanseData";
+import { processHistoryData, cleanseDataProto } from "../utils/cleanseData";
 
 export const fetchInit = dataType => {
   return {
@@ -39,21 +39,41 @@ export const invalidateData = dataType => {
 export const fetchData = (
   dataType,
   url,
-  currency,
-  keysToPick
+  currency = "",
+  keysToPick = []
 ) => async dispatch => {
   //Init fetch, the API call is starting
   dispatch(fetchInit(dataType));
 
   try {
     const result = await axios(url); //API data
-    // Should process the data here
+
     const cleansed = cleanseDataProto(result.data, currency, keysToPick);
 
     //Store data
     dispatch(receiveData(dataType, cleansed)); //Store data in the state
   } catch (error) {
-    console.log(error);
+    dispatch(fetchFailure(dataType, error));
+  }
+};
+
+export const fetchHistoryData = (
+  crypto,
+  url,
+  currency = "USD",
+  keysToPick
+) => async dispatch => {
+  const dataType = `HISTORY_${crypto}`;
+  dispatch(fetchInit(dataType));
+  try {
+    const result = await axios(url);
+    const processed = processHistoryData(
+      result.data.Data,
+      currency,
+      keysToPick
+    );
+    dispatch(receiveData(dataType, processed));
+  } catch (error) {
     dispatch(fetchFailure(dataType, error));
   }
 };
--- src/utils/cleanseData.js
+++ src/utils/cleanseData.js
@@ -1,14 +1,9 @@
 import _ from "lodash";
 
-export const cleanseData = (dataSet, currency, ...args) => {
-  const data = currency ? dataSet.RAW : _.take(dataSet.Data, 8);
-
-  const result = _.map(data, item => {
-    if (currency) {
-      return _.pick(item[currency], ...args);
-    } else {
-      return _.pick(item, ...args);
-    }
+export const processHistoryData = (dataSet, currency, ...args) => {
+  let apiData = dataSet.Data.Data;
+  const result = _.map(apiData, item => {
+    return _.pick(item, ...args);
   });
 
   //Returns an array of objects
