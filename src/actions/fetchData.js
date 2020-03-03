32d0d67892ec29d57ccac67fbaa520db2a97d054
Ainars Ciesa <ainars.c@gmail.com>
2020-03-03 01:09:34 +0000

    Add a a check before data fetching to let the app know data needs to be fetched

--- src/actions/fetchData.js
+++ src/actions/fetchData.js
@@ -40,28 +40,43 @@ export const invalidateData = dataType => {
   };
 };
 
+const shouldFetch = (state, dataType) => {
+  let data = state.dataByCategory[dataType];
+  // return true;
+  if (!data) {
+    return true;
+  } else if (data.isFetching) {
+    return false;
+  } else {
+    return data.didInvalidate;
+  }
+};
+
 export const fetchData = (
   dataType,
   url,
   crypto,
   currency,
   keysToPick = []
-) => async dispatch => {
-  dispatch(fetchInit(dataType));
+) => async (dispatch, getState) => {
+  if (shouldFetch(getState(), dataType)) {
+    dispatch(fetchInit(dataType));
+    try {
+      const result = await axios(url); //return: [instance].data
+      let processData;
+      if (dataType === "FULL_DATA") {
+        processData = cleanupFullData(result.data, keysToPick);
+      } else if (dataType === "NEWS") {
+        processData = cleanupNewsData(result.data, keysToPick);
+      } else if (dataType === "HISTORY") {
+        processData = cleanupHistoryData(result.data, crypto, currency);
+      }
 
-  try {
-    const result = await axios(url); //return: [instance].data
-    let processData;
-    if (dataType === "FULL_DATA") {
-      processData = cleanupFullData(result.data, keysToPick);
-    } else if (dataType === "NEWS") {
-      processData = cleanupNewsData(result.data, keysToPick);
-    } else if (dataType === "HISTORY") {
-      processData = cleanupHistoryData(result.data, crypto, currency);
+      dispatch(receiveData(dataType, processData));
+    } catch (error) {
+      dispatch(fetchFailure(dataType, error));
     }
-
-    dispatch(receiveData(dataType, processData));
-  } catch (error) {
-    dispatch(fetchFailure(dataType, error));
+  } else {
+    return Promise.resolve();
   }
 };
