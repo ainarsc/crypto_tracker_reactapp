d057199bb6940ec99bebc5fdb0796ea9913e6554
Ainars Ciesa <ainars.c@gmail.com>
2020-03-11 01:23:44 +0000

    Create loading bar

---
+++ src/components/ui/LoadingBar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import { connect } from "react-redux";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: "100%",
+    height: 16,
+    "& > * + *": {
+      marginTop: theme.spacing(2)
+    }
+  }
+}));
+
+function LoadingBar({ apiData }) {
+  const classes = useStyles();
+
+  return (
+    !apiData.FULL_DATA.isFetching && (
+      <div className={classes.root}>
+        <LinearProgress />
+      </div>
+    )
+  );
+}
+
+const mapState = state => ({
+  apiData: state.apiData
+});
+
+export default connect(mapState)(LoadingBar);
