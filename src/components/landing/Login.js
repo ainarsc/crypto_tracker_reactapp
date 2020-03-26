48bb7faf92b4171fd144ca1db55cabc94df717a4
Ainars Ciesa <ainars.c@gmail.com>
2020-03-26 23:57:12 +0000

    Improve login and sign up forms with more correct html markup

--- src/components/landing/Login.js
+++ src/components/landing/Login.js
@@ -3,10 +3,18 @@ import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
-import TextField from "@material-ui/core/TextField";
 import { signIn } from "../../firebase";
 import { connect } from "react-redux";
 import { useHistory } from "react-router-dom";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import FormControl from "@material-ui/core/FormControl";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import InputLabel from "@material-ui/core/InputLabel";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import IconButton from "@material-ui/core/IconButton";
+import clsx from "clsx";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -25,12 +33,21 @@ const useStyles = makeStyles(theme => ({
     flexDirection: "column",
     justifyContent: "center"
   },
+  buttonGroup: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+
+    margin: theme.spacing(2)
+  },
   text: {
     margin: theme.spacing(2)
   },
+  margin: {
+    margin: theme.spacing(2)
+  },
   button: {
-    margin: theme.spacing(2),
-    width: 150,
+    width: 120,
     alignSelf: "center",
     color: "white"
   }
@@ -41,7 +58,15 @@ const Login = ({ session }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const history = useHistory();
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+  const handleMouseDownPassword = event => {
+    event.preventDefault();
+  };
 
   const signInUser = async (event, email, password, s) => {
     event.preventDefault();
@@ -70,42 +95,87 @@ const Login = ({ session }) => {
           {message}
         </Typography>
         <Typography align="center" variant="h6" gutterBottom>
-          Login
+          Sing In
         </Typography>
-        <TextField
-          className={classes.text}
-          id="outlined-secondary"
-          label="Email Address"
+        <FormControl
+          className={clsx(classes.margin, classes.textField)}
           variant="outlined"
-          color="secondary"
-          type="text"
-          placeholder="Email address"
-          name="email"
-          value={email}
-          onChange={event => setEmail(event.target.value)}
-        />
-        <TextField
-          className={classes.text}
-          id="outlined-secondary"
-          label="Password"
-          variant="outlined"
-          color="secondary"
-          type="password"
-          placeholder="Password"
-          name="password"
-          value={password}
-          onChange={event => setPassword(event.target.value)}
-        />
-
-        <Button
-          className={classes.button}
-          variant="outlined"
-          color="secondary"
-          type="submit"
-          // onClick={() => history.push("/dashboard")}
         >
-          Sign In
-        </Button>
+          <InputLabel color="secondary" htmlFor="outlined-email">
+            Email
+          </InputLabel>
+          <OutlinedInput
+            required
+            id="outlined-email"
+            color="secondary"
+            label="Email"
+            variant="outlined"
+            name="email"
+            type="text"
+            value={email}
+            onChange={e => {
+              setEmail(e.target.value);
+            }}
+            labelWidth={46}
+          />
+        </FormControl>
+
+        <FormControl
+          className={clsx(classes.margin, classes.textField)}
+          variant="outlined"
+        >
+          <InputLabel color="secondary" htmlFor="outlined-adornment-password">
+            Password
+          </InputLabel>
+          <OutlinedInput
+            required
+            id="outlined-adornment-password"
+            color="secondary"
+            label="Password"
+            variant="outlined"
+            name="password"
+            type={showPassword ? "text" : "password"}
+            value={password}
+            onChange={e => {
+              setPassword(e.target.value);
+            }}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            }
+            labelWidth={76}
+          />
+        </FormControl>
+        <ButtonGroup
+          className={classes.buttonGroup}
+          color="secondary"
+          aria-label="outlined secondary button group"
+        >
+          <Button
+            className={classes.button}
+            variant="outlined"
+            color="secondary"
+            type="submit"
+          >
+            Sign In
+          </Button>
+          <Button
+            className={classes.button}
+            variant="outlined"
+            color="secondary"
+            onClick={() => history.push("/")}
+          >
+            Go Back
+          </Button>
+        </ButtonGroup>
       </form>
     </Container>
   );
--- src/components/landing/Register.js
+++ src/components/landing/Register.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import { makeStyles } from "@material-ui/core/styles";
-import TextField from "@material-ui/core/TextField";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import InputLabel from "@material-ui/core/InputLabel";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import FormControl from "@material-ui/core/FormControl";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import IconButton from "@material-ui/core/IconButton";
+import Button from "@material-ui/core/Button";
+import clsx from "clsx";
+import { useHistory } from "react-router-dom";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -16,43 +26,164 @@ const useStyles = makeStyles(theme => ({
 
   form: {
     margin: "0 auto",
-    width: 250
+    width: 250,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center"
+  },
+  buttonGroup: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+
+    margin: theme.spacing(2)
   },
   text: {
     margin: theme.spacing(2)
+  },
+  margin: {
+    margin: theme.spacing(2)
+  },
+  button: {
+    width: 120,
+    alignSelf: "center",
+    color: "white"
   }
 }));
 
 const Register = () => {
   const classes = useStyles();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const history = useHistory();
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+  const handleMouseDownPassword = event => {
+    event.preventDefault();
+  };
 
   return (
     <Container className={classes.root} maxWidth="sm">
-      <form className={classes.form} noValidate autoComplete="on">
+      <form className={classes.form} autoComplete="on">
         <Typography align="center" variant="h6" gutterBottom>
-          Register Profile
+          Sign Up
         </Typography>
-        <TextField
-          className={classes.text}
-          id="outlined-secondary"
-          label="Email Address"
+        <FormControl
+          className={clsx(classes.margin, classes.textField)}
           variant="outlined"
-          color="secondary"
-        />
-        <TextField
-          className={classes.text}
-          id="outlined-secondary"
-          label="Password"
+        >
+          <InputLabel color="secondary" htmlFor="outlined-email">
+            Email
+          </InputLabel>
+          <OutlinedInput
+            id="outlined-email"
+            color="secondary"
+            label="Email"
+            variant="outlined"
+            name="email"
+            type="text"
+            value={email}
+            onChange={e => {
+              setEmail(e.target.value);
+            }}
+            labelWidth={46}
+          />
+        </FormControl>
+
+        <FormControl
+          className={clsx(classes.margin, classes.textField)}
           variant="outlined"
-          color="secondary"
-        />
-        <TextField
-          className={classes.text}
-          id="outlined-secondary"
-          label="Repeat Password"
+        >
+          <InputLabel color="secondary" htmlFor="outlined-adornment-password">
+            Password
+          </InputLabel>
+          <OutlinedInput
+            required
+            id="outlined-adornment-password"
+            color="secondary"
+            label="Password"
+            variant="outlined"
+            name="password"
+            type={showPassword ? "text" : "password"}
+            value={password}
+            onChange={e => {
+              setPassword(e.target.value);
+            }}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            }
+            labelWidth={76}
+          />
+        </FormControl>
+        <FormControl
+          className={clsx(classes.margin, classes.textField)}
           variant="outlined"
+        >
+          <InputLabel color="secondary" htmlFor="outlined-adornment-password">
+            Repeat Password
+          </InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            color="secondary"
+            label="Repeat Password"
+            variant="outlined"
+            name="password"
+            type={showPassword ? "text" : "password"}
+            value={password2}
+            onChange={e => {
+              setPassword2(e.target.value);
+            }}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            }
+            labelWidth={145}
+          />
+        </FormControl>
+        <ButtonGroup
+          className={classes.buttonGroup}
           color="secondary"
-        />
+          aria-label="outlined secondary button group"
+        >
+          <Button
+            className={classes.button}
+            variant="outlined"
+            color="secondary"
+            type="submit"
+          >
+            Sign Up
+          </Button>
+          <Button
+            className={classes.button}
+            variant="outlined"
+            color="secondary"
+            onClick={() => history.push("/")}
+          >
+            Go Back
+          </Button>
+        </ButtonGroup>
       </form>
     </Container>
   );
