945b0c689834b465e12e834d6386d05f67c33275
Ainars Ciesa <ainars.c@gmail.com>
2020-04-04 00:33:54 +0000

    create action type

--- src/store/actions/setPreferences.js
+++ src/store/actions/setPreferences.js
@@ -1,8 +1,15 @@
-import { SELECT_CURRENCY } from "./actionTypes";
+import { SELECT_CURRENCY, SET_CRYPTO } from "./actionTypes";
 
 export const setCurrency = currency => {
   return {
     type: SELECT_CURRENCY,
     payload: currency
+  };
+};
+
+export const setCrypto = crypto => {
+  return {
+    type: SET_CRYPTO,
+    payload: crypto
   };
 };
