e382632d20c717d541260f8a6d9975d495609baa
Ainars Ciesa <ainars.c@gmail.com>
2020-04-19 23:38:10 +0000

    Implement redux persist

--- src/store/index.js
+++ src/store/index.js
@@ -3,26 +3,13 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import ReduxThunk from "redux-thunk";
 import logger from "redux-logger";
 import rootReducer from "./reducers";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-
-// const persistConfig = {
-//   key: "apiData",
-//   storage: storage,
-//   whitelist: ["apiData"], // which reducer want to store
-// };
-// const pReducer = persistReducer(persistConfig, rootReducer);
+import { persistStore } from "redux-persist";
 
 const middlewares = [ReduxThunk, logger];
 const composedEnhancers = composeWithDevTools(
   applyMiddleware(...middlewares)
   //Any other enhancers go here
 );
-
-//store
-// const store = createStore(rootReducer, preloadedState, composedEnhancers);
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
-// const persistor = persistStore(store);
 
 export const store = createStore(rootReducer, composedEnhancers);
 export const persistor = persistStore(store);
