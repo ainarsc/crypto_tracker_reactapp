cd3651d7dc39a3f2107aae3ada24a33f7856fcc4
Ainars Ciesa <ainars.c@gmail.com>
2020-04-19 01:40:10 +0000

    Part of store refactor

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import configureStore from "./store";
+import { store, persistor } from "./store";
 import firebaseServices, { firebaseContext } from "./firebase";
 import { ThemeProvider } from "@material-ui/styles";
 import darkTheme from "./theme";
 import { BrowserRouter } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
-
-const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
