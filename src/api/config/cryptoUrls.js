3bd5d8f73ba2760975ae589a24a9531a21be4652
Ainars Ciesa <ainars.c@gmail.com>
2020-04-25 18:04:40 +0000

    Add history url for weekly hourly data

--- src/api/config/cryptoUrls.js
+++ src/api/config/cryptoUrls.js
@@ -9,6 +9,9 @@ export const getNewsUrl = () => {
 export const getHistoryUrl = (coin, curr) => {
   return `https://min-api.cryptocompare.com/data/v2/histoday?fsym=${coin}&tsym=${curr}&allData=true`;
 };
+export const getHistoryUrlH = (coin, curr) => {
+  return `https://min-api.cryptocompare.com/data/v2/histohour?fsym=${coin}&tsym=${curr}&limit=168`;
+};
 export const getMarketDataUrl = () => {
   return `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,BCH,BSV,LTC,EOS,ETC,XTZ,BNB,ZEC,ADA,XLM,NEO,DASH&&tsyms=USD,EUR`;
 };
